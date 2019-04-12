4678230bd1ec160a044acb4eeb20ca9ba88df985
css <neetsCC7899>
2019-04-12 10:14:25 +0800

    css

--- src/router/index.js
+++ src/router/index.js
@@ -1,65 +1,3 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-
-// import Login from '@/pages/login'
-// import Layout from '@/pages/layout/layout'
-
-// Vue.use(Router)
-
-// export const constantRouterMap = [{
-//         path: '/login',
-//         name: 'login',
-//         hidden: true,
-//         component: Login,
-//         meta: {
-//             title: '登录'
-//         }
-//     },
-//     {
-//         path: '/',
-//         hidden: true,
-//         component: Layout,
-//         redirect: '/home',
-//         children: [{
-//             path: 'home',
-//             name: 'home',
-//             component: () => import('@/pages/home'),
-//             meta: {
-//                 title: '首页'
-//             }
-//         }]
-//     },
-//     {
-//         path: '/test',
-//         hidden: true,
-//         component: Layout,
-//         children: [{
-//             path: 'test1',
-//             name: 'test1',
-//             component: () => import('@/pages/test'),
-//             meta: {
-//                 title: 'test1'
-//             }
-//         },{
-//             path: 'test2',
-//             name: 'test2',
-//             component: () => import('@/pages/test2'),
-//             meta: {
-//                 title: 'test2'
-//             }
-//         }]
-//     }
-// ]
-
-// export default new Router({
-//     routes: constantRouterMap
-// })
-
-
-
-
-
-
 import Vue from 'vue'
 import Router from 'vue-router'
 
@@ -78,99 +16,99 @@ Vue.use(Router)
  */
 
 export const constantRouterMap = [{
-        path: '/login',
-        name: 'login',
-        hidden: true,
-        component: Login,
-        meta: {
-            title: '登录'
-        }
-    },
-    {
-        path: '/test',
-        component: Layout,
-        redirect: '/test/test1',
-        meta: { //控制在左栏显示
-            icon: 'question',
-            title: '测试一'
-        },
-        children: [{
-            path: 'test1',
-            name: 'test1',// 如果name设置相同会出现，刷新当前页面跳转到别的路由的情况
-            component: () => import('@/pages/test/index'),
-            meta: {
-                icon: 'warning',
-                title: '测试一'
-            }
-        },{
-            path: 'test2',
-            name: 'test2',// 如果name设置相同会出现，刷新当前页面跳转到别的路由的情况
-            component: () => import('@/pages/test2/index'),
-            meta: {
-                icon: 'warning',
-                title: '测试二'
-            }
-        }]
-    },
-    {
-        path: '/introduction',
-        component: Layout,
-        redirect: '/introduction/index',
-        meta: { //控制在左栏显示
-            icon: 'question',
-            title: '简述'
-        },
-        children: [{
-            path: 'index',
-            name: 'Introduction',
-            component: () => import('@/pages/test2/index'),
-            meta: {
-                icon: 'warning',
-                title: '简述'
-            }
-        }]
-    },
-    {
-        path: '/',
-        component: Layout,
-        redirect: '/home',
-        hidden: true,
-        children: [{
-            path: 'home',
-            name: 'home',
-            component: () => import('@/pages/home'),
-            meta: {
-                title: '首页'
-            }
-        }]
-    },
-    {
-        path: '/pc',
-        component: pc_layout,
-        hidden: true,
-        children: [{
-            path: 'pc',
-            name: 'pc',
-            component: () => import('@/pages/pc_layout'),
-            meta: {
-                title: '首页2'
-            }
-        },{
-            path: 'pc2',
-            name: 'pc2',
-            component: () => import('@/pages/pc_layout/index2'),
-            meta: {
-                title: '首页2'
-            }
-        }]
-    },
+		path: '/login',
+		name: 'login',
+		hidden: true,
+		component: Login,
+		meta: {
+			title: '登录'
+		}
+	},
+	{
+		path: '/test',
+		component: Layout,
+		redirect: '/test/test1',
+		meta: { //控制在左栏显示
+			icon: 'question',
+			title: '测试一'
+		},
+		children: [{
+			path: 'test1',
+			name: 'test1', // 如果name设置相同会出现，刷新当前页面跳转到别的路由的情况
+			component: () => import('@/pages/test/index'),
+			meta: {
+				icon: 'warning',
+				title: '测试一'
+			}
+		}, {
+			path: 'test2',
+			name: 'test2', // 如果name设置相同会出现，刷新当前页面跳转到别的路由的情况
+			component: () => import('@/pages/test2/index'),
+			meta: {
+				icon: 'warning',
+				title: '测试二'
+			}
+		}]
+	},
+	{
+		path: '/introduction',
+		component: Layout,
+		redirect: '/introduction/index',
+		meta: { //控制在左栏显示
+			icon: 'question',
+			title: '简述'
+		},
+		children: [{
+			path: 'index',
+			name: 'Introduction',
+			component: () => import('@/pages/test2/index'),
+			meta: {
+				icon: 'warning',
+				title: '简述'
+			}
+		}]
+	},
+	{
+		path: '/',
+		component: Layout,
+		redirect: '/home',
+		hidden: true,
+		children: [{
+			path: 'home',
+			name: 'home',
+			component: () => import('@/pages/home'),
+			meta: {
+				title: '首页'
+			}
+		}]
+	},
+	{
+		path: '/pc',
+		component: pc_layout,
+		hidden: true,
+		children: [{
+			path: 'pc',
+			name: 'pc',
+			component: () => import('@/pages/pc_layout'),
+			meta: {
+				title: '首页2'
+			}
+		}, {
+			path: 'pc2',
+			name: 'pc2',
+			component: () => import('@/pages/pc_layout/index2'),
+			meta: {
+				title: '首页2'
+			}
+		}]
+	},
 
 ]
 
 export default new Router({
-    // mode: 'history',  require service support
-    // scrollBehavior: () => ({ y: 0 }),
-    routes: constantRouterMap
+	// mode: 'history',  require service support
+	// scrollBehavior: () => ({ y: 0 }),
+	routes: constantRouterMap
 })
 
 export const asyncRouterMap = [
